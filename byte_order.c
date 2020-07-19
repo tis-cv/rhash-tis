61587e682dc8387e941840d9850a107f221c2e01
Mike <tankf33der@disroot.org>
2020-07-19 15:30:36 +0300

    .

--- byte_order.c
+++ byte_order.c
@@ -95,16 +95,11 @@ void rhash_swap_copy_str_to_u32(void* to, int index, const void* from, size_t le
 void rhash_swap_copy_str_to_u64(void* to, int index, const void* from, size_t length)
 {
 	/* if all pointers and length are 64-bits aligned */
-	if ( 0 == (( (int)((char*)to - (char*)0) | ((char*)from - (char*)0) | index | length ) & 7) ) {
 		/* copy aligned memory block as 64-bit integers */
 		const uint64_t* src = (const uint64_t*)from;
 		const uint64_t* end = (const uint64_t*)((const char*)src + length);
 		uint64_t* dst = (uint64_t*)((char*)to + index);
 		while (src < end) *(dst++) = bswap_64( *(src++) );
-	} else {
-		const char* src = (const char*)from;
-		for (length += index; (size_t)index < length; index++) ((char*)to)[index ^ 7] = *(src++);
-	}
 }
 
 /**
